fac22045707ad56bd8834ba899fffd6e5ec2647d
OdedWx <odedw@orbs.com>
2020-03-17 09:47:13 +0200

    descriptions and getters

--- contracts/interfaces/ICommittee.sol
+++ contracts/interfaces/ICommittee.sol
@@ -20,23 +20,24 @@ interface ICommittee {
 	function memberWeightChange(address addr, uint256 weight, bool readyForCommittee) returns (bool commiteeChanged, bool standbysChanged); /* onlyElectionContract */;
 
 	/// @dev Called by: Elections contract
-	/// Notifies a weight change for sorting to a relevant committee member.
-    /// weight = 0 indicates removal of the member from the committee (for exmaple on unregister, voteUnready, voteOut)
+	/// Notifies a a member removal for example due to voteOut / voteUnready
 	function removeMember(address addr) returns (bool commiteeChanged, bool standbysChanged); /* onlyElectionContract */;
 
 	/// @dev Called by: Elections contract
-	/// Returns the committee members and standbys
+	/// Returns the weight of 
 	function getWeight(uint N) external view returns (uint256 weight); 
 
 	/// @dev Called by: Elections contract
-	/// Returns the committee members and standbys
-	function getCommitee(uint N) external view returns (address[] memory committee, uint256[] memory standbys); 
+	/// Returns the committee members and their weights
+	function getCommitee(uint N) external view returns (address[] memory addrs, uint256[] memory weights); 
+
+	/// @dev Returns the standy (out of commiteee) members and their weights
+	function getStandbys(uint N) external view returns (address[] memory addrs, uint256[] memory weights); 
 
 	/// @dev Called by: Elections contract
 	/// Sets the mimimal weight, and committee members
     /// Every member with sortingStake >= mimimumStake OR in top minimumN is included in the committee
 	function setMinimumWeight(uint256 mimimumWeight, uint minimumN); /* onlyElectionContract */;
-
 
 	/*
 	 * Governance
@@ -45,15 +46,15 @@ interface ICommittee {
     /// @dev Updates the address calldata of the contract registry
 	function setContractRegistry(IContractRegistry _contractRegistry) external /* onlyOwner */;
 
-	/*
-	 * Getters
-	 */
+    /*
+     * Getters
+     */
 
     /// @dev returns the current committee
     /// used also by the rewards and fees contracts
-	function getCommittee() external view returns (address[] memory addr, address[] memory orbsAddr, uint32[] ip);
+	function getCommitteeInfo() external view returns (address[] memory addrs, uint256[] memory weights, address[] memory orbsAddrs, uint32[] ips);
 
     /// @dev returns the current standbys (out of commiteee) topology
-	function getStandbys() external view returns (address[] memory addr, address[] memory orbsAddr, uint32[] ip);
+	function getStandbysInfo() external view returns (address[] memory addrs, uint256[] memory weights, address[] memory orbsAddrs, uint32[] ips);
 
 }
