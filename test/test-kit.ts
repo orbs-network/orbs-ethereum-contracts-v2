41537a05fe37bae6298340f42acca1dc3af258e2
amir-arad <amir@orbs.com>
2020-03-17 13:16:57 +0200

    export all events parsing

--- test/test-kit.ts
+++ test/test-kit.ts
@@ -1,4 +1,4 @@
 export { Driver } from './driver';
 export { ganacheDriver } from './ganache-driver';
-export { subscriptionChangedEvents } from './event-parsing';
+export * from './event-parsing';
 export * from './consumer-macros';
