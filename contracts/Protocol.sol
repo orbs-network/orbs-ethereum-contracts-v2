40b326265748c7ff9064682463399d3ad760d56c
aviram <aviram@orbs.com>
2020-04-22 21:49:05 +0300

    remove redundant state reads and writes

--- contracts/Protocol.sol
+++ contracts/Protocol.sol
@@ -22,11 +22,11 @@ contract Protocol is IProtocol, Ownable {
         if (!deploymentSubsets[deploymentSubset].exists) {
             require(asOfBlock == 0, "initial protocol version must be from block 0");
             deploymentSubsets[deploymentSubset].currentVersion = protocolVersion;
+            deploymentSubsets[deploymentSubset].exists = true;
         } else {
             uint currentAsOfBlock = deploymentSubsets[deploymentSubset].asOfBlock;
-            uint currentNextVersion = deploymentSubsets[deploymentSubset].nextVersion;
             if (currentAsOfBlock <= block.number) {
-                deploymentSubsets[deploymentSubset].currentVersion = currentNextVersion;
+                deploymentSubsets[deploymentSubset].currentVersion = deploymentSubsets[deploymentSubset].nextVersion;
             }
 
             require(asOfBlock > block.number, "protocol update can only take place in the future");
@@ -36,7 +36,6 @@ contract Protocol is IProtocol, Ownable {
 
         deploymentSubsets[deploymentSubset].nextVersion = protocolVersion;
         deploymentSubsets[deploymentSubset].asOfBlock = asOfBlock;
-        deploymentSubsets[deploymentSubset].exists = true;
 
         emit ProtocolVersionChanged(deploymentSubset, protocolVersion, asOfBlock);
     }
