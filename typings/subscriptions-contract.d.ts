55d5059c8df8b27d987320d7ad3f6f81e2857222
Ron <ron@orbs.com>
2020-05-24 20:49:41 +0300

    fix typings

--- typings/subscriptions-contract.d.ts
+++ typings/subscriptions-contract.d.ts
@@ -4,14 +4,14 @@ import {TransactionConfig, TransactionReceipt} from "web3-core";
 import * as BN from "bn.js";
 
 export interface SubscriptionChangedEvent {
-  vcid: string;
+  vcid: number | BN;
   genRef: number | BN;
   expiresAt: number | BN;
   tier: string;
 }
 
 export interface PaymentEvent {
-  vcid: string;
+  vcid: number | BN;
   by: string;
   amount: number | BN;
   tier: string;
@@ -19,19 +19,19 @@ export interface PaymentEvent {
 }
 
 export interface VcConfigRecordChangedEvent {
-  vcid: string;
+  vcid: number | BN;
   key: string,
   value: string
 }
 
 export interface VcOwnerChangedEvent {
-    vcid: string;
+    vcid: number | BN;
     previousOwner: string;
     newOwner: string;
 }
 
 export interface VcCreatedEvent {
-    vcid: string;
+    vcid: number | BN;
     owner: string;
 }
 
