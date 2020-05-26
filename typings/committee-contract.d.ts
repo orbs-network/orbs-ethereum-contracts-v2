c667e5b73ab2e041aa68bcc2a7836696700b27c5
amir-arad <amir@orbs.com>
2020-05-26 20:50:17 +0300

    add ips to committee events

--- typings/committee-contract.d.ts
+++ typings/committee-contract.d.ts
@@ -6,12 +6,14 @@ export interface CommitteeChangedEvent {
     addrs: string[];
     orbsAddrs: string[];
     weights: (number | BN)[];
+    ips: string[];
 }
 
 export interface StandbysChangedEvent {
     addrs: string[];
     orbsAddrs: string[];
     weights: (number | BN)[];
+    ips: string[];
 }
 
 export interface CommitteeContract extends Contract {
