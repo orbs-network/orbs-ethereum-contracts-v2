de62ad3b44d9abc9e01f9626766552ac43b2de9e
Ron <ron@orbs.com>
2020-06-03 21:53:51 +0300

    support creating driver with existing contracts

---
+++ test/driver.spec.ts
@@ -0,0 +1,38 @@
+import 'mocha';
+
+import * as _ from "lodash";
+import BN from "bn.js";
+import {Driver, DEPLOYMENT_SUBSET_MAIN, Participant} from "./driver";
+import chai from "chai";
+import {feesAddedToBucketEvents, subscriptionChangedEvents, vcCreatedEvents} from "./event-parsing";
+import {bn, bnSum, evmIncreaseTime, fromTokenUnits, toTokenUnits} from "./helpers";
+import {TransactionReceipt} from "web3-core";
+import {Web3Driver} from "../eth";
+import {FeesAddedToBucketEvent} from "../typings/rewards-contract";
+import {Contracts} from "../typings/contracts";
+
+chai.use(require('chai-bn')(BN));
+chai.use(require('./matchers'));
+
+const expect = chai.expect;
+
+describe('testkit', async () => {
+
+  it('should instantiate a new driver object using existing contracts', async () => {
+    const firstDriver = await Driver.new({maxCommitteeSize: 4});
+    const secondDriver = await Driver.new({contractRegistryAddress: firstDriver.contractRegistry.address});
+
+    expect(firstDriver.contractRegistry.address).to.equal(secondDriver.contractRegistry.address);
+    expect(firstDriver.elections.address).to.equal(secondDriver.elections.address);
+    expect(firstDriver.erc20.address).to.equal(secondDriver.erc20.address);
+    expect(firstDriver.externalToken.address).to.equal(secondDriver.externalToken.address);
+    expect(firstDriver.staking.address).to.equal(secondDriver.staking.address);
+    expect(firstDriver.delegations.address).to.equal(secondDriver.delegations.address);
+    expect(firstDriver.subscriptions.address).to.equal(secondDriver.subscriptions.address);
+    expect(firstDriver.rewards.address).to.equal(secondDriver.rewards.address);
+    expect(firstDriver.protocol.address).to.equal(secondDriver.protocol.address);
+    expect(firstDriver.compliance.address).to.equal(secondDriver.compliance.address);
+    expect(firstDriver.validatorsRegistration.address).to.equal(secondDriver.validatorsRegistration.address);
+    expect(firstDriver.committee.address).to.equal(secondDriver.committee.address);
+  })
+});
