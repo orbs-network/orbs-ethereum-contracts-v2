f7d56db026f998e528a1bbdd172272db3ca743e7
aviram <aviram@orbs.com>
2020-06-11 09:33:31 +0300

    fix flaky test

--- test/bootstrap-rewards.spec.ts
+++ test/bootstrap-rewards.spec.ts
@@ -39,7 +39,7 @@ describe('bootstrap-rewards-level-flows', async () => {
 
     await d.rewards.setGeneralCommitteeAnnualBootstrap(annualAmountGeneral, {from: g.address});
     await d.rewards.setComplianceCommitteeAnnualBootstrap(annualAmountCompliance, {from: g.address});
-    await d.rewards.assignRewards();
+
     // create committee
 
     const initStakeLesser = fromTokenUnits(17000);
@@ -50,8 +50,12 @@ describe('bootstrap-rewards-level-flows', async () => {
     const {v: v3} = await d.newValidator(initStakeLesser, true, false, true);
     const {v: v4, r: firstAssignTxRes} = await d.newValidator(initStakeLesser, false, false, true);
     const startTime = await txTimestamp(d.web3, firstAssignTxRes);
-
     const generalCommittee: Participant[] = [v1, v2, v3, v4];
+
+    const initialBalance:BN[] = [];
+    for (const v of generalCommittee) {
+      initialBalance.push(new BN(await d.rewards.getBootstrapBalance(v.address)));
+    }
 
     await sleep(3000);
     await evmIncreaseTime(d.web3, YEAR_IN_SECONDS*4);
@@ -86,13 +90,13 @@ describe('bootstrap-rewards-level-flows', async () => {
     for (const v of generalCommittee) {
       const i = generalCommittee.indexOf(v);
 
-      const expectedBalance = (i % 2 == 0) ? expectedComplianceCommitteeRewards : expectedGeneralCommitteeRewards;
-      expect(tokenBalances[i]).to.be.bignumber.equal(expectedBalance.toString());
+      const expectedRewards = (i % 2 == 0) ? expectedComplianceCommitteeRewards : expectedGeneralCommitteeRewards;
+      expect(tokenBalances[i].sub(initialBalance[i])).to.be.bignumber.equal(expectedRewards.toString());
 
       // claim the funds
       await d.rewards.withdrawBootstrapFunds({from: v.address});
       const tokenBalance = await d.externalToken.balanceOf(v.address);
-      expect(new BN(tokenBalance)).to.bignumber.equal(new BN(expectedBalance));
+      expect(new BN(tokenBalance)).to.bignumber.equal(new BN(tokenBalances[i]));
     }
   })
 });
