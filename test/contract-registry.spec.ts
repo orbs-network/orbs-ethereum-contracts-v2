ff205cdb6e6db115bc718846d551ea933031ee9d
aviram <aviram@orbs.com>
2020-06-17 15:13:22 +0300

    more tests for coverage

--- test/contract-registry.spec.ts
+++ test/contract-registry.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 
 import BN from "bn.js";
-import {Driver, expectRejected} from "./driver";
+import {Driver, expectRejected, ZERO_ADDR} from "./driver";
 import chai from "chai";
 
 chai.use(require('chai-bn')(BN));
@@ -56,6 +56,21 @@ describe('contract-registry-high-level-flows', async () => {
 
   });
 
+  it('does not allow to set a zero address', async () => {
+    const d = await Driver.new();
+    const owner = d.functionalOwner;
+    const registry = d.contractRegistry;
+
+    // set
+    await expectRejected(registry.set("protocol", ZERO_ADDR, {from: owner.address}));
+    await registry.set("protocol", d.newParticipant().address, {from: owner.address});
+  });
+
+  it('reverts when getting a non existent entry', async () => {
+    const d = await Driver.new();
+    await expectRejected(d.contractRegistry.get("nonexistent" as any));
+  });
+
   it('allows only the contract owner to update the address of the contract registry', async () => { // TODO - consider splitting and moving this
     const d = await Driver.new();
     const subscriber = await d.newSubscriber("tier", 1);
@@ -70,6 +85,16 @@ describe('contract-registry-high-level-flows', async () => {
     await d.rewards.setContractRegistry(newAddr, {from: d.migrationOwner.address});
     await d.subscriptions.setContractRegistry(newAddr, {from: d.migrationOwner.address});
     await subscriber.setContractRegistry(newAddr, {from: d.migrationOwner.address});
+  });
+
+  it('does not allow to set a zero contract registry address', async () => { // TODO - consider splitting and moving this
+    const d = await Driver.new();
+    const subscriber = await d.newSubscriber("tier", 1);
+
+    await expectRejected(d.elections.setContractRegistry(ZERO_ADDR, {from: d.migrationOwner.address}));
+    await expectRejected(d.rewards.setContractRegistry(ZERO_ADDR, {from: d.migrationOwner.address}));
+    await expectRejected(d.subscriptions.setContractRegistry(ZERO_ADDR, {from: d.migrationOwner.address}));
+    await expectRejected(subscriber.setContractRegistry(ZERO_ADDR, {from: d.migrationOwner.address}));
   });
 
 });
