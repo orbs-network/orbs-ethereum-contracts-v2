646d1aa8e9b5eb5f76d6eccc95743a4653597a2a
aviram <aviram@orbs.com>
2020-06-28 10:24:50 +0300

    reduce boilerplate of event parsing

--- compiled-contracts.ts
+++ compiled-contracts.ts
@@ -13,4 +13,29 @@ function loadCompiledContracts(baseDir: string): CompiledContracts {
     return artifacts;
 }
 
+interface EventDefinition {
+    name: string,
+    signature: string,
+    contractName: any
+}
+
+function listEventsDefinitions(contracts: CompiledContracts): EventDefinition[] {
+    const defs: EventDefinition[] = [];
+
+    for (const contractName in contracts) {
+        const contract = contracts[contractName];
+        const eventDefs: EventDefinition[] = contract.abi
+            .filter(x => x.type == 'event')
+            .map(e => ({
+                contractName: contractName,
+                name: e.name,
+                signature: e.name + "(" + e.inputs.map(input => input.type).join(',') + ")"
+            }));
+        defs.push(...eventDefs);
+    }
+
+    return defs;
+}
+
 export const compiledContracts = loadCompiledContracts(path.join(__dirname, 'build', 'contracts'));
+export const eventDefinitions = listEventsDefinitions(compiledContracts);
