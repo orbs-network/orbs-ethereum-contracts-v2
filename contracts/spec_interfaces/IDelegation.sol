ed8e9314a09d010c2d00aff0f1d74dfad74d980c
Ron <ron@orbs.com>
2020-07-12 18:43:09 +0300

    reorder lines in interfaces/IElections.sol

--- contracts/spec_interfaces/IDelegation.sol
+++ contracts/spec_interfaces/IDelegation.sol
@@ -26,8 +26,8 @@ interface IDelegations /* is IStakeChangeNotifier */ {
     /// @dev Updates the address calldata of the contract registry
 	function setContractRegistry(IContractRegistry _contractRegistry) external /* onlyMigrationOwner */;
 
-	function importDelegations(address[] calldata from, address[] calldata to, bool notifyElections) external /* onlyMigrationOwner */;
-	function finalizeDelegationImport() external /* onlyMigrationOwner */;
+	function importDelegations(address[] calldata from, address[] calldata to, bool notifyElections) external /* onlyMigrationOwner onlyDuringDelegationImport */;
+	function finalizeDelegationImport() external /* onlyMigrationOwner onlyDuringDelegationImport */;
 
 	event DelegationsImported(address[] from, address[] to, bool notifiedElections);
 	event DelegationImportFinalized();
