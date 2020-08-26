bc31d1a12936015c3b7deae9ba14da68cbca30ef
Aviram <aviram@orbs.com>
2020-08-26 12:38:04 +0300

    - initializable private field - revert msg

--- contracts/ContractRegistry.sol
+++ contracts/ContractRegistry.sol
@@ -14,8 +14,8 @@ contract ContractRegistry is IContractRegistry, Initializable, WithClaimableRegi
 
 	mapping (string => address) managers;
 
-	modifier onlyManager {
-		require(msg.sender == registryManager() || msg.sender == initializationManager(), "sender is not an admin (registryManager or initializationManager)");
+	modifier onlyAdmin {
+		require(msg.sender == registryManager() || msg.sender == initializationManager(), "sender is not an admin (registryManager or initializationManager when initialization in progress)");
 
 		_;
 	}
@@ -29,7 +29,7 @@ contract ContractRegistry is IContractRegistry, Initializable, WithClaimableRegi
 		return previousContractRegistry;
 	}
 
-	function setContract(string calldata contractName, address addr, bool managedContract) external onlyManager {
+	function setContract(string calldata contractName, address addr, bool managedContract) external onlyAdmin {
 		require(!managedContract || addr != address(0), "managed contract may not have address(0)");
 		removeManagedContract(contracts[contractName]);
 		contracts[contractName] = addr;
@@ -40,13 +40,13 @@ contract ContractRegistry is IContractRegistry, Initializable, WithClaimableRegi
 		notifyOnContractsChange();
 	}
 
-	function lockContracts() external onlyManager {
+	function lockContracts() external onlyAdmin {
 		for (uint i = 0; i < managedContractAddresses.length; i++) {
 			ILockable(managedContractAddresses[i]).lock();
 		}
 	}
 
-	function unlockContracts() external onlyManager {
+	function unlockContracts() external onlyAdmin {
 		for (uint i = 0; i < managedContractAddresses.length; i++) {
 			ILockable(managedContractAddresses[i]).unlock();
 		}
@@ -84,7 +84,7 @@ contract ContractRegistry is IContractRegistry, Initializable, WithClaimableRegi
 		return managedContractAddresses;
 	}
 
-	function setManager(string calldata role, address manager) external onlyManager {
+	function setManager(string calldata role, address manager) external onlyAdmin {
 		managers[role] = manager;
 		emit ManagerChanged(role, manager);
 	}
@@ -93,7 +93,7 @@ contract ContractRegistry is IContractRegistry, Initializable, WithClaimableRegi
 		return managers[role]; // todo - allow zero address?
 	}
 
-	function setNewContractRegistry(IContractRegistry newRegistry) external onlyManager {
+	function setNewContractRegistry(IContractRegistry newRegistry) external onlyAdmin {
 		for (uint i = 0; i < managedContractAddresses.length; i++) {
 			IContractRegistryListener(managedContractAddresses[i]).setContractRegistry(newRegistry);
 			IContractRegistryListener(managedContractAddresses[i]).refreshContracts();
