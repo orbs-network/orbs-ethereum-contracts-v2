74f26b06c290d2b9da463b116f09962a0a8dc097
OdedWx <odedw@orbs.com>
2020-09-01 14:02:12 +0300

    cleanup

--- contracts/spec_interfaces/IDelegation.sol
+++ contracts/spec_interfaces/IDelegation.sol
@@ -5,10 +5,7 @@ import "./IContractRegistry.sol";
 /// @title Elections contract interface
 interface IDelegations /* is IStakeChangeNotifier */ {
 
-    // Delegation state change events
 	event DelegatedStakeChanged(address indexed addr, uint256 selfDelegatedStake, uint256 delegatedStake, address[] delegators, uint256[] delegatorTotalStakes);
-
-    // Function calls
 	event Delegated(address indexed from, address indexed to);
 
 	/*
@@ -18,31 +15,37 @@ interface IDelegations /* is IStakeChangeNotifier */ {
 	/// @dev Stake delegation
 	function delegate(address to) external /* onlyWhenActive */;
 
+	/// @dev updates the addr delegation and notifies the Election contract
 	function refreshStakeNotification(address addr) external /* onlyWhenActive */;
 
+	/// @dev refreshes the stake of addr from the Staking contract
 	function refreshStake(address addr) external /* onlyWhenActive */;
-
-	/*
-	 * Governance
-	 */
-
-    /// @dev Updates the address calldata of the contract registry
-	function setContractRegistry(IContractRegistry _contractRegistry) external /* onlyMigrationManager */;
-
-	function importDelegations(address[] calldata from, address[] calldata to, bool _refreshStakeNotification) external /* onlyMigrationManager onlyDuringDelegationImport */;
-	function finalizeDelegationImport() external /* onlyMigrationManager onlyDuringDelegationImport */;
-
-	event DelegationsImported(address[] from, address[] to, bool notifiedElections);
-	event DelegationImportFinalized();
 
 	/*
 	 * Getters
 	 */
 
+	/// @dev returns the delegated stake of an address
 	function getDelegatedStakes(address addr) external view returns (uint256);
+
+	/// @dev returns the self-delegated stake of an address
+	/// self-delegated stake is the self stake if self-delegated, otherwise 0.
 	function getSelfDelegatedStake(address addr) external view returns (uint256);
+	
+	/// @dev returns the current delegation of an address
 	function getDelegation(address addr) external view returns (address);
+
+	/// @dev returns the total delegated stake
+	/// The total delegated stake is the total stake delegated to self-delegated addresses
 	function getTotalDelegatedStake() external view returns (uint256) ;
 
+	/*
+	 * Migration and Initialization
+	 */
 
+	event DelegationsImported(address[] from, address[] to, bool notifiedElections);
+	event DelegationImportFinalized();
+
+	/// @dev admin function to be used during deployment of a new contract setting an initial delegation state. 
+	function importDelegations(address[] calldata from, address[] calldata to, bool _refreshStakeNotification) external /* onlyMigrationManager */;
 }
