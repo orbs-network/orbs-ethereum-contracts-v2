6345730dd191d58d8006fcd6cf1d9f327cdb913f
Ron <ron@orbs.com>
2020-09-03 19:32:03 +0300

    remove node-jq

--- extractAbi.sh
+++ extractAbi.sh
@@ -3,5 +3,5 @@ rm -rf ./release/abi
 mkdir -p ./release/abi
 for full_filename in build/contracts/*.json; do
     filename=`basename $full_filename`
-    ./node_modules/node-jq/bin/jq '.abi' ${full_filename} > ./release/abi/${filename}.abi.json
+    echo "console.log(JSON.stringify(JSON.parse(require('fs').readFileSync('./${full_filename}').toString()).abi, null, 2))" | node >  ./release/abi/${filename}.abi.json
 done
