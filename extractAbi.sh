4e1399c109b23287a08f2a0c469c34c8ecd8d5a6
Ron <ron@orbs.com>
2020-09-03 20:03:26 +0300

    slight refactor

--- extractAbi.sh
+++ extractAbi.sh
@@ -3,5 +3,6 @@ rm -rf ./release/abi
 mkdir -p ./release/abi
 for full_filename in build/contracts/*.json; do
     filename=`basename $full_filename`
-    echo "console.log(JSON.stringify(JSON.parse(require('fs').readFileSync('./${full_filename}').toString()).abi, null, 2))" | node >  ./release/abi/${filename}.abi.json
+    script="console.log(JSON.stringify(JSON.parse(require('fs').readFileSync('./${full_filename}').toString()).abi,null,2))"
+    echo $script | node >  ./release/abi/${filename}.abi.json
 done
