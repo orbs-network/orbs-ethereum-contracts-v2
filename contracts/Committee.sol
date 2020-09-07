5df82f416f4a6571edadcfa92cd60be2236d914d
Aviram <aviram@orbs.com>
2020-09-07 14:06:11 +0300

    committee refactor

--- contracts/Committee.sol
+++ contracts/Committee.sol
@@ -6,46 +6,23 @@ import "./spec_interfaces/ICommittee.sol";
 import "./spec_interfaces/IGuardiansRegistration.sol";
 import "@openzeppelin/contracts/math/Math.sol";
 import "./ContractRegistryAccessor.sol";
-import "solidity-bytes-utils/contracts/BytesLib.sol";
 import "./Lockable.sol";
 import "./interfaces/IRewards.sol";
 import "./interfaces/IElections.sol";
 import "./ManagedContract.sol";
-import "./spec_interfaces/ICertification.sol";
 
-/// @title Elections contract interface
 contract Committee is ICommittee, ManagedContract {
-	using BytesLib for bytes;
-
-	uint constant MAX_COMMITTEE_ARRAY_SIZE = 32; // Cannot be greater than 32 (number of bytes in bytes32)
-
 	struct CommitteeMember {
 		address addr;
-		uint96 weight;
+		uint96 weightAndCertifiedBit;
 	}
-	CommitteeMember[MAX_COMMITTEE_ARRAY_SIZE] public committee;
+	CommitteeMember[] public committee;
 
 	struct MemberStatus {
-		bool isMember;
+		uint32 pos;
 		bool inCommittee;
-		uint8 pos;
 	}
 	mapping (address => MemberStatus) membersStatus;
-
-	struct MemberData {
-		MemberStatus status;
-		uint96 weight;
-		bool isCertified;
-	}
-
-	// Derived properties
-	struct CommitteeInfo {
-		uint32 committeeBitmap; // TODO redundant, sort bytes can be used instead
-		uint8 minCommitteeMemberPos;
-		uint8 committeeSize;
-	}
-	CommitteeInfo committeeInfo;
-	bytes32 committeeSortBytes;
 
 	struct Settings {
 		uint32 maxTimeBetweenRewardAssignments;
@@ -59,161 +36,55 @@ contract Committee is ICommittee, ManagedContract {
 		_;
 	}
 
-	function findFreePos(CommitteeInfo memory info) private pure returns (uint8 pos) {
-		pos = 0;
-		uint32 bitmap = info.committeeBitmap;
-		while (bitmap & 1 == 1) {
-			bitmap >>= 1;
-			pos++;
-		}
+	uint96 constant CERTIFICATION_MASK = 1 << 95;
+	uint96 constant WEIGHT_MASK = ~CERTIFICATION_MASK;
+
+	function getMemberWeight(CommitteeMember memory member) private pure returns (uint256 weight) {
+		return uint256(member.weightAndCertifiedBit & WEIGHT_MASK);
 	}
 
-	function qualifiesToEnterCommittee(address addr, MemberData memory data, CommitteeInfo memory info, Settings memory _settings) private view returns (bool qualified, uint8 entryPos) {
-		if (!data.status.isMember || data.weight == 0) {
-			return (false, 0);
-		}
-
-		if (info.committeeSize < _settings.maxCommitteeSize) {
-			return (true, findFreePos(info));
-		}
-
-		CommitteeMember memory minMember = committee[info.minCommitteeMemberPos];
-		if (data.weight < minMember.weight || data.weight == minMember.weight && addr < minMember.addr) {
-			return (false, 0);
-		}
-
-		return (true, info.minCommitteeMemberPos);
+	function getMemberCertification(CommitteeMember memory member) private pure returns (bool certified) {
+		return member.weightAndCertifiedBit & CERTIFICATION_MASK != 0;
 	}
 
-	function saveMemberStatus(address addr, MemberStatus memory status) private {
-		if (status.isMember) {
-			membersStatus[addr] = status;
-		} else {
-			delete membersStatus[addr];
-		}
+	function packWeightCertification(uint256 weight, bool certification) private pure returns (uint96 weightAndCertified) {
+		return uint96(weight) | (certification ? CERTIFICATION_MASK : 0);
 	}
 
-	function updateOnMemberChange(address addr, MemberData memory data) private returns (bool committeeChanged) {
-		CommitteeInfo memory info = committeeInfo;
-		Settings memory _settings = settings;
-		bytes memory sortBytes = abi.encodePacked(committeeSortBytes);
-
-		if (!data.status.inCommittee) {
-			(bool qualified, uint8 entryPos) = qualifiesToEnterCommittee(addr, data, info, _settings);
-			if (!qualified) {
-				return false;
+	function _getMinCommitteeMember() private view returns (
+		address minMemberAddress,
+		uint256 minMemberWeight,
+		uint minMemberPos
+	){
+		CommitteeMember[] memory _committee = committee;
+		minMemberPos = uint256(-1);
+		minMemberWeight = uint256(-1);
+		uint256 memberWeight;
+		address memberAddress;
+		for (uint i = 0; i < _committee.length; i++) {
+			memberAddress = _committee[i].addr;
+			memberWeight = getMemberWeight(_committee[i]);
+			if (memberWeight < minMemberWeight || memberWeight == minMemberWeight && memberAddress < minMemberAddress) {
+				minMemberPos = i;
+				minMemberWeight = memberWeight;
+				minMemberAddress = memberAddress;
 			}
-
-			(info, sortBytes) = removeMemberAtPos(entryPos, sortBytes, info);
-			(info, sortBytes) = addToCommittee(addr, data, entryPos, sortBytes, info);
 		}
-
-		(info, sortBytes) = (data.status.isMember && data.weight > 0) ?
-			rankMember(addr, data, sortBytes, info) :
-			removeMemberFromCommittee(data, sortBytes, info);
-
-		emit GuardianCommitteeChange(addr, data.weight, data.isCertified, data.status.inCommittee);
-
-		committeeInfo = info;
-		committeeSortBytes = sortBytes.toBytes32(0);
-
-		assignRewardsIfNeeded(_settings);
-
-		return true;
 	}
 
-	function addToCommittee(address addr, MemberData memory data, uint8 entryPos, bytes memory sortBytes, CommitteeInfo memory info) private returns (CommitteeInfo memory newInfo, bytes memory newSortByes) {
-		committee[entryPos] = CommitteeMember({
-			addr: addr,
-			weight: data.weight
-		});
-		data.status.inCommittee = true;
-		data.status.pos = entryPos;
-		info.committeeBitmap |= uint32(uint(1) << entryPos);
-		info.committeeSize++;
-
-		sortBytes[info.committeeSize - 1] = byte(entryPos);
-		return (info, sortBytes);
-	}
-
-	function removeMemberFromCommittee(MemberData memory data, bytes memory sortBytes, CommitteeInfo memory info) private returns (CommitteeInfo memory newInfo, bytes memory newSortBytes) {
-		uint rank = 0;
-		while (uint8(sortBytes[rank]) != data.status.pos) {
-			rank++;
+	function qualifiesToEnterCommittee(address addr, uint256 weight, Settings memory _settings) private view returns (bool qualified, uint entryPos) {
+		uint committeeLength = committee.length;
+		if (committeeLength < _settings.maxCommitteeSize) {
+			return (true, committeeLength);
 		}
 
-		for (; rank < info.committeeSize - 1; rank++) {
-			sortBytes[rank] = sortBytes[rank + 1];
-		}
-		sortBytes[rank] = 0;
+		(address minMemberAddress, uint256 minMemberWeight, uint minMemberPos) = _getMinCommitteeMember();
 
-		info.committeeSize--;
-		if (info.committeeSize > 0) {
-			info.minCommitteeMemberPos = uint8(sortBytes[info.committeeSize - 1]);
-		}
-		info.committeeBitmap &= ~uint32(uint(1) << data.status.pos);
-
-		delete committee[data.status.pos];
-		data.status.inCommittee = false;
-
-		return (info, sortBytes);
-	}
-
-	function removeMemberAtPos(uint8 pos, bytes memory sortBytes, CommitteeInfo memory info) private returns (CommitteeInfo memory newInfo, bytes memory newSortBytes) {
-		if (info.committeeBitmap & (uint(1) << pos) == 0) {
-			return (info, sortBytes);
+		if (weight > minMemberWeight || weight == minMemberWeight && addr > minMemberAddress) {
+			return (true, minMemberPos);
 		}
 
-		CommitteeMember memory cm = committee[pos];
-
-		MemberData memory data = MemberData({
-			status: MemberStatus({
-				pos: pos,
-				inCommittee: true,
-				isMember: true
-			}),
-			weight: cm.weight,
-			isCertified: certificationContract.isGuardianCertified(cm.addr)
-		});
-
-		(newInfo, newSortBytes) = removeMemberFromCommittee(data, sortBytes, info);
-
-		emit GuardianCommitteeChange(cm.addr, data.weight, data.isCertified, false);
-
-		membersStatus[cm.addr] = data.status;
-	}
-
-	function rankMember(address addr, MemberData memory data, bytes memory sortBytes, CommitteeInfo memory info) private view returns (CommitteeInfo memory newInfo, bytes memory newSortBytes) {
-		uint rank = 0;
-		while (uint8(sortBytes[rank]) != data.status.pos) {
-			rank++;
-		}
-
-		CommitteeMember memory cur = CommitteeMember({addr: addr, weight: data.weight});
-		CommitteeMember memory next;
-
-		while (rank < info.committeeSize - 1) {
-			next = committee[uint8(sortBytes[rank + 1])];
-			if (cur.weight > next.weight || cur.weight == next.weight && cur.addr > next.addr) break;
-
-			(sortBytes[rank], sortBytes[rank + 1]) = (sortBytes[rank + 1], sortBytes[rank]);
-			rank++;
-		}
-
-		while (rank > 0) {
-			next = committee[uint8(sortBytes[rank - 1])];
-			if (cur.weight < next.weight || cur.weight == next.weight && cur.addr < next.addr) break;
-
-			(sortBytes[rank], sortBytes[rank - 1]) = (sortBytes[rank - 1], sortBytes[rank]);
-			rank--;
-		}
-
-		info.minCommitteeMemberPos = uint8(sortBytes[info.committeeSize - 1]);
-		return (info, sortBytes);
-	}
-
-	function getMinCommitteeMemberWeight() external view returns (uint96) {
-		return committee[committeeInfo.minCommitteeMemberPos].weight;
+		return (false, 0);
 	}
 
 	function assignRewardsIfNeeded(Settings memory _settings) private {
@@ -239,66 +110,83 @@ contract Committee is ICommittee, ManagedContract {
 	 */
 
 	function memberChange(address addr, uint256 weight, bool isCertified) external override onlyElectionsContract onlyWhenActive returns (bool committeeChanged) {
-		require(uint256(uint96(weight)) == weight, "weight is out of range");
+		MemberStatus memory status = membersStatus[addr];
 
-		MemberData memory data = MemberData({
-			status: membersStatus[addr],
-			weight: uint96(weight),
-			isCertified: isCertified
-		});
-
-		if (!data.status.isMember) {
+		if (!status.inCommittee) {
 			return false;
 		}
 
-		if (data.status.inCommittee) {
-			committee[data.status.pos].weight = data.weight;
-		}
-
-		committeeChanged = updateOnMemberChange(addr, data);
-		if (committeeChanged) {
-			saveMemberStatus(addr, data.status);
-		}
+		committee[status.pos].weightAndCertifiedBit = packWeightCertification(weight, isCertified);
+		emit GuardianCommitteeChange(addr, weight, isCertified, true);
+		assignRewardsIfNeeded(settings);
+		return true;
 	}
 
 	function addMember(address addr, uint256 weight, bool isCertified) external override onlyElectionsContract onlyWhenActive returns (bool committeeChanged) {
-		require(uint256(uint96(weight)) == weight, "weight is out of range");
+		Settings memory _settings = settings;
+		MemberStatus memory status = membersStatus[addr];
 
-		if (membersStatus[addr].isMember) {
+		if (status.inCommittee) {
 			return false;
 		}
 
-		MemberData memory data = MemberData({
-			status: MemberStatus({
-				isMember: true,
-				inCommittee: false,
-				pos: uint8(-1)
-			}),
-			weight: uint96(weight),
-			isCertified: isCertified
+		(bool qualified, uint entryPos) = qualifiesToEnterCommittee(addr, weight, _settings);
+		if (!qualified) {
+			return false;
+		}
+
+		CommitteeMember memory newMember = CommitteeMember({
+			addr: addr,
+			weightAndCertifiedBit: packWeightCertification(weight, isCertified)
 		});
-		committeeChanged = updateOnMemberChange(addr, data);
-		saveMemberStatus(addr, data.status);
+
+		if (entryPos < committee.length) {
+			CommitteeMember memory evicted = committee[entryPos];
+			delete membersStatus[evicted.addr];
+			emit GuardianCommitteeChange(evicted.addr, getMemberWeight(evicted), getMemberCertification(evicted), false);
+
+			committee[entryPos] = newMember;
+		} else {
+			committee.push(newMember);
+		}
+
+		status.inCommittee = true;
+		status.pos = uint32(entryPos);
+		membersStatus[addr] = status;
+
+		emit GuardianCommitteeChange(addr, weight, isCertified, true);
+		assignRewardsIfNeeded(_settings);
+		return true;
+	}
+
+	function _removeMember(address addr) private returns (bool committeeChanged) {
+		MemberStatus memory status = membersStatus[addr];
+		if (!status.inCommittee) {
+			return false;
+		}
+
+		CommitteeMember memory member = committee[status.pos];
+		delete membersStatus[addr];
+
+		uint committeeLength = committee.length;
+
+		if (status.pos < committeeLength - 1) {
+			CommitteeMember memory last = committee[committeeLength - 1];
+			committee[status.pos] = last;
+			membersStatus[last.addr].pos = status.pos;
+		}
+		committee.pop();
+
+		emit GuardianCommitteeChange(addr, getMemberWeight(member), getMemberCertification(member), false);
+
+		assignRewardsIfNeeded(settings);
+		return true;
 	}
 
 	/// @dev Called by: Elections contract
 	/// Notifies a a member removal for example due to voteOut / voteUnready
 	function removeMember(address addr) external override onlyElectionsContract onlyWhenActive returns (bool committeeChanged) {
-		MemberStatus memory status = membersStatus[addr];
-		if (!status.isMember) {
-			return false;
-		}
-
-		status.isMember = false;
-
-		MemberData memory data = MemberData({
-			status: status,
-			weight: status.inCommittee ? committee[status.pos].weight : uint96(electionsContract.getEffectiveStake(addr)),
-			isCertified: certificationContract.isGuardianCertified(addr)
-		});
-		committeeChanged = updateOnMemberChange(addr, data);
-
-		saveMemberStatus(addr, status);
+		return _removeMember(addr);
 	}
 
 	/// @dev Called by: Elections contract
@@ -308,27 +196,16 @@ contract Committee is ICommittee, ManagedContract {
 	}
 
 	function _getCommittee() private view returns (address[] memory addrs, uint256[] memory weights, bool[] memory certification) {
-		CommitteeInfo memory _committeeInfo = committeeInfo;
-		uint bitmap = uint(_committeeInfo.committeeBitmap);
-		uint committeeSize = _committeeInfo.committeeSize;
+		CommitteeMember[] memory _committee = committee;
+		addrs = new address[](_committee.length);
+		weights = new uint[](_committee.length);
+		certification = new bool[](_committee.length);
 
-		addrs = new address[](committeeSize);
-		weights = new uint[](committeeSize);
-		uint aInd = 0;
-		uint pInd = 0;
-		CommitteeMember memory member;
-		bitmap = uint(_committeeInfo.committeeBitmap);
-		while (bitmap != 0) {
-			if (bitmap & 1 == 1) {
-				member = committee[pInd];
-				addrs[aInd] = member.addr;
-				weights[aInd] = member.weight;
-				aInd++;
-			}
-			bitmap >>= 1;
-			pInd++;
+		for (uint i = 0; i < _committee.length; i++) {
+			addrs[i] = _committee[i].addr;
+			weights[i] = getMemberWeight(_committee[i]);
+			certification[i] = getMemberCertification(_committee[i]);
 		}
-		certification = certificationContract.getGuardiansCertification(addrs);
 	}
 
 	/*
@@ -344,25 +221,23 @@ contract Committee is ICommittee, ManagedContract {
 		return settings.maxTimeBetweenRewardAssignments;
 	}
 
+	function getMinCommitteeMember() external view returns (address addr, uint256 weight) {
+		(addr, weight, ) = _getMinCommitteeMember();
+	}
+
 	function setMaxCommitteeSize(uint8 maxCommitteeSize) public override onlyFunctionalManager /* todo onlyWhenActive */ {
 		require(maxCommitteeSize > 0, "maxCommitteeSize must be larger than 0");
-		require(maxCommitteeSize <= MAX_COMMITTEE_ARRAY_SIZE, "maxCommitteeSize must be 32 at most");
 		Settings memory _settings = settings;
-		emit MaxCommitteeSizeChanged(maxCommitteeSize, _settings.maxCommitteeSize);
+		uint8 prevMaxCommitteeSize = _settings.maxCommitteeSize;
 		_settings.maxCommitteeSize = maxCommitteeSize;
 		settings = _settings;
 
-		CommitteeInfo memory info = committeeInfo;
-		if (maxCommitteeSize >= info.committeeSize) {
-			return;
+		while (committee.length > maxCommitteeSize) {
+			(address minMemberAddress, ,) = _getMinCommitteeMember();
+			_removeMember(minMemberAddress);
 		}
 
-		bytes memory sortBytes = abi.encodePacked(committeeSortBytes);
-		for (int rank = int(info.committeeSize); rank >= int(maxCommitteeSize); rank--) {
-			(info, sortBytes) = removeMemberAtPos(uint8(sortBytes[uint(rank)]), sortBytes, info);
-		}
-		committeeInfo = info;
-		committeeSortBytes = sortBytes.toBytes32(0);
+		emit MaxCommitteeSizeChanged(maxCommitteeSize, prevMaxCommitteeSize);
 	}
 
 	function getMaxCommitteeSize() external override view returns (uint8) {
@@ -401,12 +276,10 @@ contract Committee is ICommittee, ManagedContract {
 	IElections electionsContract;
 	IRewards rewardsContract;
 	IGuardiansRegistration guardianRegistrationContract;
-	ICertification certificationContract;
 	function refreshContracts() external override {
 		electionsContract = IElections(getElectionsContract());
 		rewardsContract = IRewards(getRewardsContract());
 		guardianRegistrationContract = IGuardiansRegistration(getGuardiansRegistrationContract());
-		certificationContract = ICertification(getCertificationContract());
 	}
 
 }
