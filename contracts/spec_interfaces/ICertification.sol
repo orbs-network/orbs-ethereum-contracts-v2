56ef9658b85e9e1b610b65ada20f07094d12022e
OdedWx <odedw@orbs.com>
2020-10-25 14:14:57 +0200

    natspec

--- contracts/spec_interfaces/ICertification.sol
+++ contracts/spec_interfaces/ICertification.sol
@@ -15,8 +15,8 @@ interface ICertification /* is Ownable */ {
 	function isGuardianCertified(address guardian) external view returns (bool isCertified);
 
 	/// Sets the guardian certification status
-	/// @dev governance function called only by the functional manager
+	/// @dev governance function called only by the certification manager
 	/// @param guardian - the guardian to query
 	/// @param isCertified bool indication wether the guardian is certified
-	function setGuardianCertification(address guardian, bool isCertified) external /* onlyFunctionalManager */ ;
+	function setGuardianCertification(address guardian, bool isCertified) external /* onlyCertificationManager */ ;
 }
