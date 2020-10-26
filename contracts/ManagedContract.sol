6153f1fad66866e7d98e9e4361bc78a705dada7a
Aviram <aviram@orbs.com>
2020-10-26 15:45:12 +0200

    migraiton manager is lock owner

--- contracts/ManagedContract.sol
+++ contracts/ManagedContract.sol
@@ -8,18 +8,6 @@ contract ManagedContract is Lockable {
 
     constructor(IContractRegistry _contractRegistry, address _registryAdmin) Lockable(_contractRegistry, _registryAdmin) public {}
 
-    modifier onlyMigrationManager {
-        require(isManager("migrationManager"), "sender is not the migration manager");
-
-        _;
-    }
-
-    modifier onlyFunctionalManager {
-        require(isManager("functionalManager"), "sender is not the functional manager");
-
-        _;
-    }
-
     function refreshContracts() virtual external {}
 
 }
