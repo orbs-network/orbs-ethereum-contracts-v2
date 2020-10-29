9718120941754c30c2765cb732befe05616a0520
Aviram <aviram@orbs.com>
2020-10-29 12:38:59 +0200

    consistent interfaces

---
+++ contracts/spec_interfaces/IManagedContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+interface IManagedContract /* is ILockable, IContractRegistryAccessor, Initializable */ {
+
+    /// Refreshes the address of the other contracts the contract interacts with
+    /// @dev called by the registry contract upon an update of a contract in the registry
+    function refreshContracts() external;
+
+}
